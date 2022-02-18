d6e81a3078658ab69d44730b89d7e0bbfefa8526
Riley Croker <riley.croker@vikings.berry.edu>
2022-02-17 21:51:02 -0600

    finishing touches

--- player.py
+++ player.py
@@ -28,5 +28,6 @@ class Player:
         if self.timeSinceLastBullet > self.timeBetweenBullets:
             self.canFire = True
             self.timeSinceLastBullet = 0
+            
     def setDead(self, aBool):
         self.isDead = aBool    
